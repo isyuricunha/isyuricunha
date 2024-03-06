e5ce8df86324a4ce018791339d3930cb46b460ef
Yuri Cunha <isyuricunha@duck.com>
2024-03-06 14:29:27 -0300

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -31,7 +31,7 @@ i solve complex data management challenges by designing, implementing, and maint
 [website](https://yuricunha.com), [quick links](https://links.yuricunha.com), [meeting with me](https://cal.com/isyuricunha)
 
 **link with me:**<br>
-[x/twitter](https://twitter.com/isyuricunha), [readcv](https://read.cv/isyuricunha), [telegram](https://t.me/isyuricunha), [mail](mailto:me@yuricunha.com), [boo](https://signup.boo.world/jejk), [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)
+[x/twitter](https://twitter.com/isyuricunha), [readcv](https://read.cv/isyuricunha), [mail](mailto:isyuricunha@duck.com), [boo](https://signup.boo.world/jejk), [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)
 
 **last two blog post:**<br>
 [One month after leaving the internet and leaving everyone believing I'm still active
