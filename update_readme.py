d633764ede7d8d2aa0ef86f8b964698fb7adc828
Yuri Cunha <isyuricunha@duck.com>
2024-04-20 17:55:43 -0300

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -34,8 +34,7 @@ i solve complex data management challenges by designing, implementing, and maint
 [x/twitter](https://twitter.com/isyuricunha), [readcv](https://read.cv/isyuricunha), [mail](mailto:isyuricunha@duck.com), [boo](https://signup.boo.world/jejk), [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)
 
 **last two blog post:**<br>
-[I'm proud of you](https://yuricunha.com/blog/im-proud-of-you) and [I was born in the wrong generation
-](https://yuricunha.com/blog/i-was-born-in-the-wrong-generation)
+[Im glad you’re here. Dont go anywhere](https://yuricunha.com/blog/im-glad-youre-here-dont-go-anywhere) and [I'm proud of you](https://yuricunha.com/blog/im-proud-of-you)
 
 **podcast and last ep:**<br>
 [yuri cunha - blog speech](https://open.spotify.com/show/2XRQ2mpUbtT0ZqxFVrl0KK) and [eu nasci na geração errada](https://open.spotify.com/episode/720qm2ZXIGVodsPvDUYjU5)
