c798593e090ac174eec66cba65430a2f737113c8
Yuri Cunha <isyuricunha@duck.com>
2024-05-14 13:52:28 -0300

    Update blog post links in update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -37,7 +37,7 @@ i solve complex data management challenges by designing, implementing, and maint
 [x/twitter](https://twitter.com/isyuricunha), [readcv](https://read.cv/isyuricunha), [mail](mailto:isyuricunha@duck.com), [boo](https://signup.boo.world/jejk), [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)
 
 **last two blog post:**<br>
-[Im glad you’re here. Dont go anywhere](https://yuricunha.com/blog/im-glad-youre-here-dont-go-anywhere) and [I'm proud of you](https://yuricunha.com/blog/im-proud-of-you)
+[navigating contradictions in life](https://yuricunha.com/blog/navigating-contradictions-in-life) and [im glad you’re here. dont go anywhere](https://yuricunha.com/blog/im-glad-youre-here-dont-go-anywhere)
 
 **podcast and last ep:**<br>
 [yuri cunha - blog speech](https://open.spotify.com/show/2XRQ2mpUbtT0ZqxFVrl0KK) and [eu nasci na geração errada](https://open.spotify.com/episode/720qm2ZXIGVodsPvDUYjU5)
