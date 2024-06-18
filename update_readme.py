b0ee9976b5ea013779cd330d232c83204d615647
Yuri Cunha <isyuricunha@duck.com>
2024-06-18 13:41:38 -0300

    alter my main mail
    me@yuricunha.com

--- update_readme.py
+++ update_readme.py
@@ -31,7 +31,7 @@ i solve complex data management challenges by designing, implementing, and maint
 [website](https://yuricunha.com) and [quick links](https://links.yuricunha.com)
 
 **link with me:**<br>
-[x/twitter](https://twitter.com/isyuricunha), [readcv](https://read.cv/isyuricunha), [mail](mailto:isyuricunha@duck.com), [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)
+[x/twitter](https://twitter.com/isyuricunha), [readcv](https://read.cv/isyuricunha), [mail](mailto:me@yuricunha.com), [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)
 
 **blog posts:**<br>
 [website blog](https://yuricunha.com/blog/reflections-of-a-conflicted-impostor) & [bear blog](https://yuricunha.bearblog.dev/)
