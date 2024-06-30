2c7ca648987bce2411eca91b747620405da7fa89
Yuri <isyuricunha@duck.com>
2024-06-30 15:22:42 -0300

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -27,7 +27,7 @@ def generate_markdown_sentence(line):
 
 I solve complex data management challenges by designing, implementing, and maintaining high-performance, scalable, and secure database, cloud, and server/hardware solutions
 
-if you usa any of my services, you can check the status of service here:<br>
+if you use any of my services, you can check the status of service here:<br>
 [status page](https://status-page.yuricunha.com/)
 
 **find me on the web:**<br>
@@ -36,7 +36,7 @@ if you usa any of my services, you can check the status of service here:<br>
 **link with me:**<br>
 [x/twitter](https://twitter.com/isyuricunha), [readcv](https://read.cv/isyuricunha), [mail](mailto:me@yuricunha.com), [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)
 
-**blog posts:**<br>
+**blogs:**<br>
 [website blog](https://yuricunha.com/blog/reflections-of-a-conflicted-impostor) & [bear blog](https://yuricunha.bearblog.dev/)
 
 **a sentence to brighten your day:**<br>
