5b3053e4f2a7d0183d53d7055cb2c0c4c124da88
Yuri <isyuricunha@duck.com>
2025-04-16 08:55:17 -0300

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -3,10 +3,10 @@ import json
 import random
 
 def download_json():
-    FILE_ID  = '1MYhLPeAoTMzpuCHDVeMbRfx-LQ16uQ92'
-    FILE_URL = f'https://drive.google.com/uc?id={FILE_ID}'
+    file_id  = '1MYhLPeAoTMzpuCHDVeMbRfx-LQ16uQ92'
+    file_url = f'https://drive.google.com/uc?id={file_id}'
     
-    response = requests.get(FILE_URL)
+    response = requests.get(file_url)
     with open('./quotes.json', 'wb') as quotes_file:
         quotes_file.write(response.content)
 
@@ -23,9 +23,9 @@ def read_lines_from_json():
 
 def generate_markdown_sentence(line):
     return f'''
-**cloud and infrastructure specialist**
+**database administrator**
 
-i design, implement, and maintain robust, scalable, and secure infrastructure solutions for servers, clouds, and enterprise environments. with a focus on reliability and performance, i ensure systems run efficiently and effectively, solving challenges from hardware to the cloud.
+i manage, configure, and optimize databases and data systems to ensure high availability, data integrity, and optimal performance. i develop effective backup, recovery, and security strategies to protect critical information.
 
 **find me on the web:**<br>
 [personal website](https://yuricunha.com/?utm_source=github.com) | [quick links](https://links.yuricunha.com)
@@ -35,11 +35,11 @@ i design, implement, and maintain robust, scalable, and secure infrastructure so
 - [read.cv](https://read.cv/isyuricunha)  
 - [email (personal)](mailto:me@yuricunha.com)  
 - [email (work)](mailto:contact@yuricunha.com)  
-- [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)  
+- [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)
 
 **blogs:**<br>
 - [website blog](https://yuricunha.com/blog/?utm_source=github.com)  
-- [bear blog](https://yuricunha.bearblog.dev/)  
+- [bear blog](https://yuricunha.bearblog.dev/)
 
 **a sentence to brighten your day:**<br>
     {line}
@@ -59,4 +59,4 @@ def main():
     write_readme(markdown_sentence)
 
 if __name__ == "__main__":
-    main();
+    main()
