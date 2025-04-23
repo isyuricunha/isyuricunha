3c49b7799f95dd2b2ed04e540fe70a92a574832a
Yuri <isyuricunha@duck.com>
2025-04-23 16:23:15 -0300

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -32,7 +32,6 @@ i manage, configure, and optimize databases and data systems to ensure high avai
 
 **connect with me:**<br>
 - [x/twitter](https://twitter.com/isyuricunha)  
-- [read.cv](https://read.cv/isyuricunha)  
 - [email (personal)](mailto:me@yuricunha.com)  
 - [email (work)](mailto:contact@yuricunha.com)  
 - [spotify](https://open.spotify.com/user/22wrcoowop6hb63heywvtaypy?si=e1e818483a1a43a1)
